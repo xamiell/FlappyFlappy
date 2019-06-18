a71dd756f81344338f383467af225b729e34e4b4
Elias Pujols <eliasnurbs@gmail.com>
2019-06-18 09:33:40 -0400

    Added to the game manager the control over time of difficult of the game

--- Assets/Scripts/ColumnGenerator.cs
+++ Assets/Scripts/ColumnGenerator.cs
@@ -10,6 +10,8 @@ public class ColumnGenerator : MonoBehaviour
         public const float YPOSMAX = 2f;
     }
 
+    public static int GeneratorRate;
+
     [SerializeField] GameSettings gameSettings;
     [SerializeField] GameObject columns;
     [SerializeField] Transform wall;
@@ -21,15 +23,15 @@ public class ColumnGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GeneratorRate = gameSettings.columnGeneratorRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _frameCounter = ( _frameCounter.Equals( gameSettings.columnGeneratorRate ) ) ? _frameCounter = default : _frameCounter + 1;
+        _frameCounter = ( _frameCounter.Equals( GeneratorRate ) ) ? _frameCounter = default : _frameCounter + 1;
 
-        if ( PlayerReadyDetector.IsPlayerReady && _frameCounter.Equals( gameSettings.columnGeneratorRate ) )
+        if ( PlayerReadyDetector.IsPlayerReady && _frameCounter.Equals( GeneratorRate ) )
         {
             _currentColumns = Instantiate( columns, new Vector3( gameObject.transform.position.x, Random.Range( YPositions.YPOSMIN, YPositions.YPOSMAX ), 
                 gameObject.transform.position.z ), Quaternion.identity ) as GameObject;
